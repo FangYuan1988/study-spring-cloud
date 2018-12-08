9fafce9a28f0f332da4538d04317893af036feb0
fang <yuan.fang1988@gmail.com>
2018-12-08 16:17:40 +0800

    完成了: *  Setting up a Configuration Server * Retrieving Configuration with the Config Client

---
+++ config-client-app/src/main/java/com/fang/configclientapp/ConfigClientAppConfiguration.java
@@ -0,0 +1,18 @@
+package com.fang.configclientapp;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties(prefix = "some")//找到some开头的资源
+public class ConfigClientAppConfiguration {
+    private String property;
+
+    public String getProperty() {
+        return property;
+    }
+
+    public void setProperty(String property) {
+        this.property = property;
+    }
+}
